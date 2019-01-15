f3959ba0893b6600ff5f91bda4e70a9438c0989e
Kelly-James <kellyjamesclancy@gmail.com>
2019-01-14 18:29:24 -0800

    added state to App, state being updated with weather data

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,47 @@ import Weather from './Weather';
 import '../css/App.css';
 
 class App extends Component {
+
+  state = {
+    main: {
+      temp: null,
+      temp_max: null,
+      temp_min: null
+    },
+    sys: {
+      sunrise: null,
+      sunset: null
+    },
+    weather: {
+      description: null,
+      main: null
+    },
+    wind: {
+      deg: null,
+      speed: null
+    }
+  }
   
   componentDidMount() {
     console.log('App Mounted..');
   }
   
-  fetchWeatherData(cityId) {
+  fetchWeatherData = (cityId) => {
     const apiKey = require('../keys/api-key.json');
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey.api_key}`)
-    .then(response => response.json())
-    .then(response => console.log('Weather Response: ', response))
+    // fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey.api_key}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&APPID=${apiKey.api_key}`)
+      .then(response => response.json())
+      // .then(response => console.log("Weather Response: ", response))
+      .then(response => this.dissectData(response));
+  }
+
+  dissectData = (responseData) => {
+    this.setState( { 
+      main: { ...this.state.main, temp: responseData.main.temp },
+      sys: { ...this.state.sys, sunrise: responseData.sys.sunrise }, 
+      weather: { ...this.state.weather, description: responseData.weather[0].description },
+      wind: { ...this.state.wind, deg: responseData.wind.deg }
+    });
   }
   
   render() {
